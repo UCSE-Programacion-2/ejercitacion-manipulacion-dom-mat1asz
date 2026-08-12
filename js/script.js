b55d791c30ce35df0f12ff42ba5bbff7e472e93d
Matias <matias2004camposxd@gmail.com>
2026-08-11 21:15:14 -0300

    feat(js): agregar clase card a la tarjeta con setAttribute
    Closes #2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 //1- Agregar a la tarjeta, el atributo "class" con el valor "card"
-
+const tarjeta = document.getElementById('tarjeta');
+tarjeta.setAttribute('class', 'card');
 
 //2- Agregar a la imagen, el atributo "src" con el valor "https://www.youtube.com/img/desktop/yt_1200.png"
 
-
 //3- Quitarle al titulo la clase que le está dando un formato feo
 
-
 //4- Chequear si el link a youtube posee o no el atributo href
-
 
 //5- Obtener el href del link a wikipedia y mostrarlo por consola
